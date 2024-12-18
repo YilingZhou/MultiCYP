13e49be3cd0269ec324223174b193df24200590e
Shalom <142876768+YilingZhou@users.noreply.github.com>
2024-12-18 11:18:35 +0800

    Update draw.py

--- SOM/draw.py
+++ SOM/draw.py
@@ -54,7 +54,7 @@ def score_to_color(score, transparency=1):
     # Ensure transparency is within valid range
     transparency = max(0.0, min(1.0, transparency))
     # Calculate adjusted transparency based on score
-    adjusted_transparency = (score-0.6) * 2 * transparency
+    adjusted_transparency = abs(score-0.6) * 2 * transparency
     # Return RGBA color with adjusted transparency
     return (1.0, 0.0, 0.0, adjusted_transparency)
 
@@ -73,86 +73,87 @@ def draw_molecules_with_scores(molecule_list, file_path, img_size=(500, 450)):
     """
     # Calculate the total height for the SVG canvas
     height_per_molecule = img_size[1] + 60
-    total_height = height_per_molecule * len(molecule_list)
-    # Initialize the global SVG canvas
-    global_svg = f'<svg width="{img_size[0]}" height="{total_height}" xmlns="http://www.w3.org/2000/svg">\n'
-
-    for index, (title, smiles, atom_scores, bond_scores) in enumerate(molecule_list):
-        # Convert SMILES string to molecule object and compute 2D coordinates
-        mol = Chem.MolFromSmiles(smiles)
-        AllChem.Compute2DCoords(mol)
-
-        # Create drawer object for each molecule
-        drawer = rdMolDraw2D.MolDraw2DSVG(img_size[0], img_size[1])
-
-        # Create color highlights based on scores for atoms and bonds
-        highlight_atom_colors = {i: score_to_color(score[0], 0.8) for i, score in atom_scores.items()}
-        highlight_atoms = list(highlight_atom_colors.keys())
-        highlight_bond_colors = {i: score_to_color(score[0], 0.8) for i, score in bond_scores.items()}
-        highlight_bonds = list(highlight_bond_colors.keys())
-
-        # Modify drawing options
-        options = drawer.drawOptions()
-        options.addAtomIndices = False
-        options.atomLabelFontSize = 16
-        options.bondLineWidth = 3
-        options.highlightBondWidthMultiplier = 4
-        options.continuousHighlight = True
-        options.setHighlightColour((1, 0, 0, 0.8))
-        options.backgroundColour = (1, 1, 1)
-
-        # Draw the molecule with highlights
-        rdMolDraw2D.PrepareAndDrawMolecule(
-            drawer, mol, highlightAtoms=highlight_atoms, highlightAtomColors=highlight_atom_colors,
-            highlightBonds=highlight_bonds, highlightBondColors=highlight_bond_colors
-        )
-
-        # Finish drawing and retrieve SVG text
-        drawer.FinishDrawing()
-        svg_fragment = drawer.GetDrawingText()
-
-        # Remove XML header declaration
-        svg_fragment = '\n'.join(svg_fragment.split('\n')[1:])
-
-        # Add labels for atoms based on their scores and types
-        atom_offset = {'dx': 10, 'dy': -10}
-        for i, (score, atom_type) in atom_scores.items():
-            pos = drawer.GetDrawCoords(i)
-            text_label = f'<text x="{pos.x + atom_offset["dx"]}" y="{pos.y + atom_offset["dy"]}" ' \
-                         f'font-size="16" font-family="Arial" fill="black" text-anchor="start">' \
-                         f'{score:.2f}\n{atom_type}</text>'
-            svg_fragment = svg_fragment.replace('</svg>', f'{text_label}</svg>', 1)
-
-            # Add labels for bonds based on their scores and types
-        bond_offset = {'dx': 10, 'dy': 10}
-        for i, (score, bond_type) in bond_scores.items():
-            bond = mol.GetBondWithIdx(i)
-            begin_atom_idx = bond.GetBeginAtomIdx()
-            end_atom_idx = bond.GetEndAtomIdx()
-            begin_pos = drawer.GetDrawCoords(begin_atom_idx)
-            end_pos = drawer.GetDrawCoords(end_atom_idx)
-            mid_pos = Point2D((begin_pos.x + end_pos.x) / 2, (begin_pos.y + end_pos.y) / 2)
-            text_label = f'<text x="{mid_pos.x + bond_offset["dx"]}" y="{mid_pos.y + bond_offset["dy"]}" ' \
-                         f'font-size="16" font-family="Arial" fill="black" text-anchor="start">' \
-                         f'{score:.2f}\n{bond_type}</text>'
-            svg_fragment = svg_fragment.replace('</svg>', f'{text_label}</svg>', 1)
-
-            # Add title above each molecule image
-        title_y_position = height_per_molecule * index + 30
-        title_text = f'<text x="{img_size[0] / 2}" y="{title_y_position}" font-size="18" font-family="Arial" fill="black" text-anchor="middle">{title}</text>\n'
-        svg_fragment = f'<g transform="translate(0, {height_per_molecule * index + 60})">\n{svg_fragment}\n</g>\n'
-
-        # Append the title and molecule SVG fragment to the global SVG
-        global_svg += title_text + svg_fragment
-
-    global_svg += '</svg>'
-
-    # Write the global SVG to a file with UTF-8 encoding
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(global_svg)
-
-    #     # Display the generated SVG in the notebook
-    # display(SVG(global_svg))
+    if molecule_list != None: 
+      total_height = height_per_molecule * len(molecule_list)
+      # Initialize the global SVG canvas
+      global_svg = f'<svg width="{img_size[0]}" height="{total_height}" xmlns="http://www.w3.org/2000/svg">\n'
+  
+      for index, (title, smiles, atom_scores, bond_scores) in enumerate(molecule_list):
+          # Convert SMILES string to molecule object and compute 2D coordinates
+          mol = Chem.MolFromSmiles(smiles)
+          AllChem.Compute2DCoords(mol)
+  
+          # Create drawer object for each molecule
+          drawer = rdMolDraw2D.MolDraw2DSVG(img_size[0], img_size[1])
+  
+          # Create color highlights based on scores for atoms and bonds
+          highlight_atom_colors = {i: score_to_color(score[0], 0.8) for i, score in atom_scores.items()}
+          highlight_atoms = list(highlight_atom_colors.keys())
+          highlight_bond_colors = {i: score_to_color(score[0], 0.8) for i, score in bond_scores.items()}
+          highlight_bonds = list(highlight_bond_colors.keys())
+  
+          # Modify drawing options
+          options = drawer.drawOptions()
+          options.addAtomIndices = False
+          options.atomLabelFontSize = 16
+          options.bondLineWidth = 3
+          options.highlightBondWidthMultiplier = 4
+          options.continuousHighlight = True
+          options.setHighlightColour((1, 0, 0, 0.8))
+          options.backgroundColour = (1, 1, 1)
+  
+          # Draw the molecule with highlights
+          rdMolDraw2D.PrepareAndDrawMolecule(
+              drawer, mol, highlightAtoms=highlight_atoms, highlightAtomColors=highlight_atom_colors,
+              highlightBonds=highlight_bonds, highlightBondColors=highlight_bond_colors
+          )
+  
+          # Finish drawing and retrieve SVG text
+          drawer.FinishDrawing()
+          svg_fragment = drawer.GetDrawingText()
+  
+          # Remove XML header declaration
+          svg_fragment = '\n'.join(svg_fragment.split('\n')[1:])
+  
+          # Add labels for atoms based on their scores and types
+          atom_offset = {'dx': 10, 'dy': -10}
+          for i, (score, atom_type) in atom_scores.items():
+              pos = drawer.GetDrawCoords(i)
+              text_label = f'<text x="{pos.x + atom_offset["dx"]}" y="{pos.y + atom_offset["dy"]}" ' \
+                           f'font-size="16" font-family="Arial" fill="black" text-anchor="start">' \
+                           f'{score:.2f}\n{atom_type}</text>'
+              svg_fragment = svg_fragment.replace('</svg>', f'{text_label}</svg>', 1)
+  
+              # Add labels for bonds based on their scores and types
+          bond_offset = {'dx': 10, 'dy': 10}
+          for i, (score, bond_type) in bond_scores.items():
+              bond = mol.GetBondWithIdx(i)
+              begin_atom_idx = bond.GetBeginAtomIdx()
+              end_atom_idx = bond.GetEndAtomIdx()
+              begin_pos = drawer.GetDrawCoords(begin_atom_idx)
+              end_pos = drawer.GetDrawCoords(end_atom_idx)
+              mid_pos = Point2D((begin_pos.x + end_pos.x) / 2, (begin_pos.y + end_pos.y) / 2)
+              text_label = f'<text x="{mid_pos.x + bond_offset["dx"]}" y="{mid_pos.y + bond_offset["dy"]}" ' \
+                           f'font-size="16" font-family="Arial" fill="black" text-anchor="start">' \
+                           f'{score:.2f}\n{bond_type}</text>'
+              svg_fragment = svg_fragment.replace('</svg>', f'{text_label}</svg>', 1)
+  
+              # Add title above each molecule image
+          title_y_position = height_per_molecule * index + 30
+          title_text = f'<text x="{img_size[0] / 2}" y="{title_y_position}" font-size="18" font-family="Arial" fill="black" text-anchor="middle">{title}</text>\n'
+          svg_fragment = f'<g transform="translate(0, {height_per_molecule * index + 60})">\n{svg_fragment}\n</g>\n'
+  
+          # Append the title and molecule SVG fragment to the global SVG
+          global_svg += title_text + svg_fragment
+  
+      global_svg += '</svg>'
+  
+      # Write the global SVG to a file with UTF-8 encoding
+      with open(file_path, 'w', encoding='utf-8') as file:
+          file.write(global_svg)
+  
+      #     # Display the generated SVG in the notebook
+      # display(SVG(global_svg))
 
 
 def draw_save(pred, var, enzymes=[],savepath=''):

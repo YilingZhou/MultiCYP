10bc72600120c8b48c5ca5e46f3928bfb3de9fd4
Shalom <142876768+YilingZhou@users.noreply.github.com>
2024-12-18 10:16:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,10 +21,7 @@ setup(
     },
     python_requires=">=3.8",
     install_requires=[
-        "torch>=1.8.0",
         "rdkit",
-        "numpy>=1.19.0",
-        "pandas>=1.3.0",
-        "scikit-learn>=0.24.0"
+        "chemprop",
     ]
 )
